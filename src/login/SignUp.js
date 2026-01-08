f68063f4f6a8be1c175997739787270a5bee5a1c
johnanishgitc <john@itcatalystindia.com>
2026-01-08 22:13:45 +0530

    Login issue

--- src/login/SignUp.js
+++ src/login/SignUp.js
@@ -211,8 +211,9 @@ function SignUp() {
                 style={{
                   position: 'absolute',
                   left: 12,
-                  top: emailFocused || email ? '-10px' : '10px',
-                  fontSize: emailFocused || email ? 14 : 15,
+                  top: emailFocused || email ? '-12px' : '10px',
+                  fontSize: emailFocused || email ? 12 : 15,
+                  zIndex: 10,
                   fontWeight: 600,
                   color: '#60a5fa',
                   backgroundColor: '#fff',
@@ -403,8 +404,9 @@ function SignUp() {
                   style={{
                     position: 'absolute',
                     left: 12,
-                    top: emailFocused || email ? '-10px' : '10px',
-                    fontSize: emailFocused || email ? 14 : 15,
+                    top: emailFocused || email ? '-12px' : '10px',
+                    fontSize: emailFocused || email ? 12 : 15,
+                    zIndex: 10,
                     fontWeight: 600,
                     color: '#60a5fa',
                     backgroundColor: '#fff',
